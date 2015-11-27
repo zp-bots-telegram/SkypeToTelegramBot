1f3bbf28b19f1ea8422710e4527be45a64221bc0
Zack Pollard <iam@zackpollard.pro>
2015-11-27 03:05:20 +0000

    Issues with loading extra chats for now.

--- src/main/java/pro/zackpollard/telegrambot/skypetotelegrambot/managers/SkypeManager.java
+++ src/main/java/pro/zackpollard/telegrambot/skypetotelegrambot/managers/SkypeManager.java
@@ -138,7 +138,6 @@ public class SkypeManager {
 
             Skype skype = new SkypeBuilder(username, password).withAllResources().build();
             skype.login();
-            skype.loadMoreChats(10);
             skype.getEventDispatcher().registerListener(new SkypeEventsListener(instance, telegramBot, telegramUserID));
             skype.subscribe();
 
